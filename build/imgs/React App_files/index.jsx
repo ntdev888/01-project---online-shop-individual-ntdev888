9fc0ff4fb55f74d38911c5017f8b7ebe353536e2
Nic Tau <nicholas.tauroa@developersinstitute.co.nz>
2023-09-06 15:54:32 +1200

    production

---
+++ build/imgs/React App_files/index.jsx
@@ -0,0 +1,15 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=1b1fde60"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=1b1fde60"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom from "/node_modules/.vite/deps/react-dom.js?v=1b1fde60"; const ReactDOM = __vite__cjsImport2_reactDom.__esModule ? __vite__cjsImport2_reactDom.default : __vite__cjsImport2_reactDom;
+import App from "/src/App.jsx?t=1692509186584";
+ReactDOM.render(/* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "/home/dinictauroa/Coding/DI/semester-1/01-project---online-shop-individual-ntdev888/src/index.jsx",
+  lineNumber: 5,
+  columnNumber: 5
+}, this) }, void 0, false, {
+  fileName: "/home/dinictauroa/Coding/DI/semester-1/01-project---online-shop-individual-ntdev888/src/index.jsx",
+  lineNumber: 4,
+  columnNumber: 17
+}, this), document.getElementById("root"));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBTUk7QUFOSixPQUFPQSxXQUFXO0FBQ2xCLE9BQU9DLGNBQWM7QUFDckIsT0FBT0MsU0FBUztBQUVoQkQsU0FBU0UsT0FDUCx1QkFBQyxNQUFNLFlBQU4sRUFDQyxpQ0FBQyxTQUFEO0FBQUE7QUFBQTtBQUFBO0FBQUEsT0FBSSxLQUROO0FBQUE7QUFBQTtBQUFBO0FBQUEsT0FFQSxHQUNBQyxTQUFTQyxlQUFlLE1BQU0sQ0FDaEMiLCJuYW1lcyI6WyJSZWFjdCIsIlJlYWN0RE9NIiwiQXBwIiwicmVuZGVyIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCJdLCJzb3VyY2VzIjpbImluZGV4LmpzeCJdLCJmaWxlIjoiL2hvbWUvZGluaWN0YXVyb2EvQ29kaW5nL0RJL3NlbWVzdGVyLTEvMDEtcHJvamVjdC0tLW9ubGluZS1zaG9wLWluZGl2aWR1YWwtbnRkZXY4ODgvc3JjL2luZGV4LmpzeCIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBSZWFjdCBmcm9tIFwicmVhY3RcIjtcbmltcG9ydCBSZWFjdERPTSBmcm9tIFwicmVhY3QtZG9tXCI7XG5pbXBvcnQgQXBwIGZyb20gXCIuL0FwcFwiO1xuXG5SZWFjdERPTS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPixcbiAgZG9jdW1lbnQuZ2V0RWxlbWVudEJ5SWQoXCJyb290XCIpXG4pO1xuIl19
